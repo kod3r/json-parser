2dc75f9f43219208268f9da4579ba6e82a6218a4
Marek Jelen <marek@jelen.biz>
2013-04-28 16:21:15 +0200

    2nd take

--- json.rb
+++ json.rb
@@ -5,50 +5,50 @@ class JSON < Parslet::Parser
   rule(:space) { (match('\s')).repeat(1) }
   rule(:space?) { space.maybe }
 
-  rule(:string) { 
-  	str('"') >>
-  	(  ( str('\\') >> any ) | ( str('"').absent? >> any ) ).repeat.as(:string) >>
-  	str('"')
+  rule(:string) {
+    str('"') >>
+    ((str('\\') >> any) | (str('"').absent? >> any)).repeat.as(:string) >>
+    str('"')
   }
 
   rule(:digit) { match('[0-9]') }
 
-  rule(:number) { 
-  	str('-').maybe >> 
-  	( str('0') | ( match('[1-9]') >> digit.repeat ) ) >> 
-  	( str('.') >> digit.repeat(1) ).maybe >> 
-  	(
-  		( str('e') | str('E') ) >>
-  		( str('+') | str('-') ).maybe >>
-  		digit.repeat(1)
-  	).maybe
+  rule(:number) {
+    str('-').maybe >>
+    (str('0') | (match('[1-9]') >> digit.repeat)) >>
+    (str('.') >> digit.repeat(1)).maybe >>
+    (
+      (str('e') | str('E')) >>
+      (str('+') | str('-')).maybe >>
+      digit.repeat(1)
+    ).maybe
   }
 
-  rule(:value) { space? >> 
-    ( 
-      string | 
-      number.as(:number) | 
-      object | 
-      array | 
-      str('true').as(:true) | 
-      str('false').as(:false) | 
-      str('null').as(:null) 
-    ) >> 
-    space? 
+  rule(:value) { space? >>
+    (
+      string |
+      number.as(:number) |
+      object |
+      array |
+      str('true').as(:true) |
+      str('false').as(:false) |
+      str('null').as(:null)
+    ) >>
+    space?
   }
 
-  rule(:array) { 
-    str('[') >> 
-    ( value >> ( str(',') >> value ).repeat.maybe ).maybe.as(:array) >> 
-    space? >> 
+  rule(:array) {
+    str('[') >>
+    (value >> (str(',') >> value).repeat.maybe).maybe.as(:array) >>
+    space? >>
     str(']')
   }
 
-  rule(:member) { 
-    space? >> 
-    string.as(:name) >> 
-    space? >> 
-    str(':') >> 
+  rule(:member) {
+    space? >>
+    string.as(:name) >>
+    space? >>
+    str(':') >>
     value.as(:value)
   }
 
@@ -64,9 +64,9 @@ class Ruby < Parslet::Transform
   rule(:object => subtree(:object)) {
     case object
       when Hash
-        { object[:name] => object[:value] }
+        {object[:name] => object[:value]}
       when Array
-        object.inject({}) do |l,o|
+        object.inject({}) do |l, o|
           l[o[:name]] = o[:value]; l
         end
       else
@@ -74,7 +74,7 @@ class Ruby < Parslet::Transform
     end
   }
 
-  rule(:array => subtree(:array)) { 
+  rule(:array => subtree(:array)) {
     case
       when array.respond_to?(:to_a)
         array.to_a
@@ -83,7 +83,7 @@ class Ruby < Parslet::Transform
     end
   }
 
-  rule(:string => simple(:string)) { 
+  rule(:string => simple(:string)) {
     s = string.to_s
     s.gsub!('\"', '"')
     s.gsub!('\\\\', '\\')
@@ -102,5 +102,5 @@ class Ruby < Parslet::Transform
   rule(:true => simple(:true)) { true }
   rule(:false => simple(:false)) { false }
   rule(:null => simple(:null)) { nil }
-  
+
 end
